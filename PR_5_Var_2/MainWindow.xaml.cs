c3974f5a36ab85559f692c773172ba8c4b9bd9fd
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-09-30 18:19:55 +0300

    Files

--- PR_5_Var_2/MainWindow.xaml.cs
+++ PR_5_Var_2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -16,11 +17,161 @@ using System.Windows.Shapes;
 
 namespace PR_5_Var_2
 {
+    struct MoneyString 
+    { 
+        public string rubl;
+        public string kop;
+        public string dollar;
+        public string euro;
+        public string rupi;
+        public string frank;
+        public string ien;
+        public string concat()
+        {
+            return rubl + ";" + kop + ";" + dollar + ';' + euro + ';' + rupi + ';' + frank + ';' + ien;
+        }
+    }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        void getData(string path, List<MoneyString> L)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string[] array = sr.ReadLine().Split(';');
+                    AddItem(L, array[0], array[1], array[2], array[3], array[4], array[5], array[6]);
+                }
+            }
+        }
+        static void PrintToFile(string path, List<MoneyString> L)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (MoneyString u in L)
+                {
+                    sw.WriteLine(u.concat());
+                }
+            }
+        }
+        void AddItem(List<MoneyString> L, string rubl, string kop, string dollar, string euro, string rupi, string frank, string ien)
+        {
+            L.Add(new MoneyString()
+            {
+                rubl = rubl,
+                kop = kop,
+                dollar = dollar,
+                euro = euro,
+                rupi = rupi,
+                frank = frank,
+                ien = ien,
+            });
+        }
+        void WorkWithData(List<MoneyString> L, List<MoneyString> L1)
+        {
+            foreach(MoneyString m in L)
+            {
+                if ((m.rubl != "") || (m.kop != ""))
+                {
+                    if (IsRightRusFile(m.rubl))
+                    {
+                        if (IsRightRub(m.rubl))
+                        {
+                            if (IsRightRusFile(m.kop))
+                            {
+                                if (IsRightKop(m.kop))
+                                {
+                                    string str = m.rubl + "," + m.kop;
+                                    double rus = Convert.ToDouble(str);
+                                    double dollar = rus / 58.8;
+                                    double euro = rus / 56.32;
+                                    double rupi = rus / 0.71;
+                                    double frank = rus / 59;
+                                    double ien = rus / 0.4;
+                                    AddItem(L1, "", "", dollar.ToString(), euro.ToString(), rupi.ToString(), frank.ToString(), ien.ToString());
+                                }
+    
+                                
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (m.dollar != "")
+                    {
+                        if (IsRightIn(m.dollar))
+                        {
+                            double dollar = Convert.ToDouble(m.dollar);
+                            dollar *= 58.08;
+                            double kop = Math.Round(dollar * 100, 0) % 100;
+                            dollar = Math.Truncate(dollar);
+                            AddItem(L1, dollar.ToString(), kop.ToString(), "", "", "", "", "");
+                        }
+                    }
+                    else
+                    {
+                        if (m.euro != "")
+                        {
+                            if (IsRightIn(m.euro))
+                            {
+                                double euro = Convert.ToDouble(m.euro);
+                                euro *= 56.32;
+                                double kop = Math.Round(euro * 100, 0) % 100;
+                                euro = Math.Truncate(euro);
+                                AddItem(L1, euro.ToString(), kop.ToString(), "", "", "", "", "");
+                            }
+                        }
+                        else
+                        {
+                            if (m.rupi != "")
+                            {
+                                if (IsRightIn(m.rupi))
+                                {
+                                    double rupi = Convert.ToDouble(m.rupi);
+                                    rupi *= 0.71;
+                                    double kop = Math.Round(rupi * 100, 0) % 100;
+                                    rupi = Math.Truncate(rupi);
+                                    AddItem(L1, rupi.ToString(), kop.ToString(), "", "", "", "", "");
+                                }
+                            }
+                            else
+                            {
+                                if (m.frank != "")
+                                {
+                                    if (IsRightIn(m.frank))
+                                    {
+                                        double frank = Convert.ToDouble(m.frank);
+                                        frank *= 59;
+                                        double kop = Math.Round(frank * 100, 0) % 100;
+                                        frank = Math.Truncate(frank);
+                                        AddItem(L1, frank.ToString(), kop.ToString(), "", "", "", "", "");
+                                    }
+                                }
+                                else
+                                {
+                                    if (m.ien != "")
+                                    {
+                                        if (IsRightIn(m.ien))
+                                        {
+                                            double ien = Convert.ToDouble(m.ien);
+                                            ien *= 0.4;
+                                            double kop = Math.Round(ien * 100, 0) % 100;
+                                            ien = Math.Truncate(ien);
+                                            AddItem(L1, ien.ToString(), kop.ToString(), "", "", "", "", "");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         bool IsRightIn(string s)
         {
             Regex r = new Regex("^[0-9]+[.,]?[0-9]*$");
@@ -52,6 +203,43 @@ namespace PR_5_Var_2
             else
             {
                 MessageBox.Show("Введено некорректное значение!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+        bool IsRightRusFile(string s)
+        {
+            Regex r = new Regex("^[0-9]*$");
+            if (r.IsMatch(s))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Введено некорректное значение!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+        bool IsRightRub(string s)
+        {
+            if (Convert.ToInt32(s) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Рубли должны быть больше нуля", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+        bool IsRightKop(string s)
+        {
+            if (Convert.ToInt32(s) <= 100)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Копеек не может быть больше 100", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
             }
         }
@@ -174,11 +362,11 @@ namespace PR_5_Var_2
             {
                 if (IsRightRus(tbRub.Text))
                 {
-                    if(Convert.ToInt32(tbRub.Text) > 0)
+                    if(IsRightRub(tbRub.Text))
                     {
                         if (IsRightRus(tbKop.Text))
                         {
-                            if(Convert.ToInt32(tbKop.Text) <= 100)
+                            if(IsRightKop(tbKop.Text))
                             {
                                 if (cbIn.SelectedIndex != -1)
                                 {
@@ -233,15 +421,7 @@ namespace PR_5_Var_2
                                     MessageBox.Show("Выберите значение из выпадающего списка", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                                 }
                             }
-                            else
-                            {
-                                MessageBox.Show("Копеек не может быть больше 100", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                            }
                         }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Рубли должны быть больше нуля", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                 }
             }
@@ -261,6 +441,15 @@ namespace PR_5_Var_2
                 stButton.Visibility = Visibility.Collapsed;
                 stOtv.Visibility = Visibility.Collapsed;
             }
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            List<MoneyString> money = new List<MoneyString>();
+            getData("dataMoney.csv", money);
+            List<MoneyString> m1 = new List<MoneyString>();
+            WorkWithData(money, m1);
+            PrintToFile("dataMoneyNew.csv", m1);
         }
     }
 }
